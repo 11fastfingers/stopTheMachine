939d5978771fb3c3b8752185e5ab957ce79b22bd
James Spencer <jamez.sp8ncer@gmail.com>
2025-06-14 08:01:44 +1000

    bigger text

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -1,43 +1,48 @@
 import React from 'react';
+import './Index.css';
 
 function Index() {
     return (
         <div>
-            <h1>Stop the Machine</h1>
-            <h2>AI will take our jobs.</h2>
-            <p> In ten years time, 15% to 50% of our jobs will be gone.</p>
+            <div id="text">
+                <h1>Stop the Machine</h1>
+                <h2>AI will take our jobs.</h2>
+                <p> In ten years time, 15% to 50% of our jobs will be gone.</p>
 
-            <h2>AI will uproot the pillars of society.</h2>
-            <p> Over the next 20 years, the chance of major AI disruption: 
-                <ul> 
-                    <li>90% news and media</li>
-                    <li>80% education</li>
-                    <li>60% legal system</li>
-                    <li>40% government</li>
-                </ul>
-            </p>
-            <h2>AI will wipe out humanity. </h2>
-            <p> AI is the greatest existential threat to humanity. 1% - 90% chance that AI will cause human extinction over the next 100 years.
-            </p>
-            
-            <h2>Time is running out</h2>
-            <p> We have 5 to 40 years before Artifical General Intelligence is created. Once that happens, it's game over. 
-                Humans become irrelevant, and likely extinct.
-            </p>
+                <h2>AI will uproot the pillars of society.</h2>
+                <p> Over the next 20 years, the chance of major AI disruption: 
+                    <ul> 
+                        <li>90% news and media</li>
+                        <li>80% education</li>
+                        <li>60% legal system</li>
+                        <li>40% government</li>
+                    </ul>
+                </p>
+                <h2>AI could wipe out humanity. </h2>
+                <p> AI is the greatest existential threat to humanity. 1% - 90% chance that AI will cause human extinction over the next 100 years.
+                </p>
+                
+                <h2>Time is running out</h2>
+                <p> We have 5 to 40 years before Artifical General Intelligence is created. Once that happens, it's game over. 
+                    Humans become irrelevant, and possibly extinct.
+                </p>
 
-            <h2> What can we do about it?</h2>
-            <ol>
-                <li> Spread the word</li>
-                <li> Don't use AI or AI affiliated products</li>
-                <li> Vote with our dollars.</li>
-                <li> Contact our governments.</li>
-                <li> Donate to stopthemachine.org. </li>
-            </ol>
-            
+                <h2> What can we do about it?</h2>
+                <ol>
+                    <li> Spread the word</li>
+                    <li> Don't use AI or AI affiliated products</li>
+                    <li> Vote with our dollars.</li>
+                    <li> Contact our governments.</li>
+                    <li> Donate to stopthemachine.org. </li>
+                    <li> Volunteer to stopthemachine.org. </li>
+                </ol>
+                
 
-            <button>Donate</button>
-            <h2>Donation Leaderboard</h2>
-            <h2>Contributors Leaderboard</h2>
+                <button>Donate</button>
+                <h2>Donation Leaderboard</h2>
+                <h2>Donation Spending</h2>
+                <h2>Contributors Leaderboard</h2>
+            </div>
         </div>
     );
 }
