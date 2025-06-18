beeda879241bf730efe75e2f2130f07f59b82a76
James Spencer <jamez.sp8ncer@gmail.com>
2025-06-18 13:09:59 +1000

    corrected base url

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 let BASE_URL; 
 if (process.env.NODE_ENV === "production") {
-    BASE_URL = "https://stopthemachine.org"; 
+    BASE_URL = "https://stop-the-machine.onrender.com/"; 
 }
 else {
     BASE_URL = "http://localhost:5173"; 
