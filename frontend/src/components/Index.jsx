30527622984a25b89769e8cda9b81b0087555ca2
James Spencer <jamez.sp8ncer@gmail.com>
2025-07-18 11:21:19 +1000

    about page created

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -21,8 +21,6 @@ function Index() {
 
 
 
-
-
     const [openDropdowns, setOpenDropdowns] = useState({
         Info: false,
         terms: false,
@@ -52,6 +50,15 @@ function Index() {
         </div>
     );
 
+
+    const aboutAction = () => {
+        const landing = document.getElementById('landing-page');
+        const about = document.getElementById('about-page');
+      
+        landing.style.display = 'none';
+        about.style.display = 'block';
+        window.scrollTo({ top: 0});
+    }
 
 
     
@@ -116,7 +123,7 @@ function Index() {
     return (
         <div>
             <div id="landing-page">
-                <div id="text">
+                <div className="text">
                     <h1>Stop the Machine</h1>
                     <h2>AI will take our jobs</h2>
                     <p> In 10 years time, up to 50% of our jobs could vanish as AI replaces human labour across industries.</p>
@@ -210,14 +217,55 @@ function Index() {
 
                         </div>
                         <div id="about-container">
-                            <a id="about"> about</a>
+                            <a id="about" onClick={aboutAction} > about</a>
                         </div>
                         
                 </div>
 
             </div>
-            <div id="about-page">
+            <div id="about-page" style={{display: 'none'}}>
+                <div className="text"> 
+                    <h1>About</h1>
 
+                    <h2>The Architect</h2>
+                    <p> Hi, I'm James. I like sweets, reading, and spending time in nature.</p>
+                    
+                    <img></img>
+                    
+                    <p> I'm 20 years old, and from Australia - but don't hold that against me. I don't drink alcohol, for health reasons. I've never had a single drop my entire life!
+                        I don't follow sports. In terms of famous sports people, I couldn't name more than the fingers on one hand. I also squat on the toilet. Yes, you read that right. I perch up like a bird on the toilet seat. I read somewhere that 
+                        it's more "efficient" that way... 
+                    </p>
+
+                    <img ></img>
+                    
+                    <h2> How StopTheMachine Operates</h2>
+                    <p> A couple of important things to mention. First, i'm the person behind StopTheMachine. Everything here is personally created and maintained by me - and only me. That means I do all the designing, the writing, the programming, the advertising, ect. 
+                        And it will always be just me - nomatter what. So the point of this is too keep operating costs as low as possible, so that 100% of donations go to online awareness ads. Minor expenses like web hosting and that sort of stuff, that's very cheap and is covered by me personally.    
+                    </p>
+
+                    <p>
+                        In terms of how it works, basically I do one simple thing. I take in donations and use those donations to run online ads warning people about the risks of AI. That's it. Every donation and every ad purchase is logged publically, so you can match things up. Additionally, I screen record every single ad purchase I make. Those screen recordings are also publically available.  
+                        
+
+                    </p>
+
+                    <h2> Legal Status</h2> 
+                        <p>
+                        Another thing to mention: StopTheMachine is the non-profit initiative of an Australian sole-trader registered under the name "Beta Products". This means that I am personally responsible for the promise that 100% of donations go to online ads. I have skin in the game. Any misappropriation of donations - to so much as buy a cup of coffee
+                        - could send me to jail. My personal assets could be seized, ect ect.  
+                        </p>
+                        <p>
+                        Unfortunately, as I am not registered as a charity donations are not tax deductible. If I was a charity, that would mean an expensive board of directors, less control, and less speed. This way is simpler and cheaper.
+                        In regards to the relationship between Beta and StopTheMachine, basically it works like this. If I need to I can transfer money from Beta to StopTheMachine. However, money cannot go the other way around. Another thing, so all parts of StopTheMachine are in the public domain. They can be freely used, shared, copied, whatever. The code is also open source. 
+                        However, this does not apply to other parts of Beta, which are proprietary. 
+                        </p>
+
+
+
+                </div>
+
+                
             </div>
         </div>
     );
