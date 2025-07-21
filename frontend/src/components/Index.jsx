b77bd50088cad1fbe5bf825852f10548da6e175a
James Spencer <jamez.sp8ncer@gmail.com>
2025-07-22 09:22:33 +1000

    removed disclaimer

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -556,10 +556,14 @@ function Index() {
 
                     <h2> Legal Disclaimer</h2>
 
+                    {/*
                     <p>
                         Ok, so one thing I said was that every $1 donated warns 200 people. That was an approximation. $1 gets you from 100 to 600 impressions on Reddit. And in some cases people might be shown the same ad multiple times. 
                         Also if you are donating a small amount, there will be less impact. E.g donating 50c won't warn 100 people, because Stripe takes 30c as it's transaction fee. 
                     </p>
+                    
+                    */ }
+                    
 
                     <p>
                         On a final note, all StopTheMachine does is raise awareness about the risks of AI. I take donations, I buy ads. That is it - nothing else. <b>StopTheMachine is not involved in activism </b> of any kind, nor is any particular solution endorsed. 
