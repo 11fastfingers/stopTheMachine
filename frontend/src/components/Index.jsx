2f7f07153965cd83b3510b4cbe5a38518e0493e7
James Spencer <jamez.sp8ncer@gmail.com>
2025-07-22 11:31:40 +1000

    simplification

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -420,7 +420,7 @@ function Index() {
 
                     <p>
                         <b>Donations launch 22 August, 2025, 15:00 UCT. </b> 100% of donations go to online awareness ads warning people about the risks of AI. Any additional operating expenses are covered by me personally, or separately funded.  
-                        Every donation and every ad purchase will be logged publicly. Additionally, I will screen record every single ad purchase I make and put those up.  
+                        Every donation and every ad purchase will be logged publicly. Additionally, I will screen record every single ad purchase.  
                     </p>
 
                        
